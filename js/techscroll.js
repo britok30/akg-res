bb413123cef948eacbbc4e86ab1c73b1d61b5a66
Kelvin Brito <britok30@gmail.com>
2020-07-14 21:42:53 -0400

    remove content

--- js/techscroll.js
+++ js/techscroll.js
@@ -21,40 +21,7 @@ ScrollReveal().reveal(".location", {
     distance: "10px",
     delay: 1000,
 });
-ScrollReveal().reveal("video", {
-    duration: 2000,
-    origin: "bottom",
-    distance: "10px",
-    viewFactor: 0.2,
-});
-ScrollReveal().reveal(".hg-list", {
-    duration: 2000,
-    origin: "top",
-    distance: "10px",
-});
-ScrollReveal().reveal(".exclusive", {
-    duration: 2000,
-    origin: "bottom",
-    distance: "10px",
-});
-ScrollReveal().reveal("#showcase h2, p", {
-    duration: 2000,
-    origin: "bottom",
-    distance: "10px",
-    delay: 1000,
-});
-ScrollReveal().reveal("#gallery", {
-    duration: 2000,
-    origin: "bottom",
-    distance: "10px",
-    delay: 1500,
-});
-ScrollReveal().reveal("#showcase-2 h2, p", {
-    duration: 2000,
-    origin: "bottom",
-    distance: "10px",
-    delay: 1000,
-});
+
 ScrollReveal().reveal("#feature .f-logo", {
     duration: 2000,
     origin: "bottom",
