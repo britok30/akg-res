b32edc3d0a356c1216ae01addc9323eb104fb28a
Kelvin Brito <britok30@gmail.com>
2020-07-14 21:28:19 -0400

    changes

--- js/techscroll.js
+++ js/techscroll.js
@@ -1,69 +1,69 @@
 ScrollReveal().reveal(".hero-lead", {
     duration: 2000,
     origin: "bottom",
-    distance: "1rem",
+    distance: "10px",
     delay: 1000,
 });
 ScrollReveal().reveal(".navbar", {
     duration: 2000,
     origin: "bottom",
-    distance: "1rem",
+    distance: "10px",
 });
 ScrollReveal().reveal(".about-us", {
     duration: 2000,
     origin: "top",
-    distance: "1rem",
+    distance: "10px",
     delay: 1000,
 });
 ScrollReveal().reveal(".location", {
     duration: 2000,
     origin: "top",
-    distance: "1rem",
+    distance: "10px",
     delay: 1000,
 });
 ScrollReveal().reveal("video", {
     duration: 2000,
     origin: "bottom",
-    distance: "1rem",
+    distance: "10px",
     viewFactor: 0.2,
 });
 ScrollReveal().reveal(".hg-list", {
     duration: 2000,
     origin: "top",
-    distance: "1rem",
+    distance: "10px",
 });
 ScrollReveal().reveal(".exclusive", {
     duration: 2000,
     origin: "bottom",
-    distance: "1rem",
+    distance: "10px",
 });
 ScrollReveal().reveal("#showcase h2, p", {
     duration: 2000,
     origin: "bottom",
-    distance: "1rem",
+    distance: "10px",
     delay: 1000,
 });
 ScrollReveal().reveal("#gallery", {
     duration: 2000,
     origin: "bottom",
-    distance: "1rem",
+    distance: "10px",
     delay: 1500,
 });
 ScrollReveal().reveal("#showcase-2 h2, p", {
     duration: 2000,
     origin: "bottom",
-    distance: "1rem",
+    distance: "10px",
     delay: 1000,
 });
 ScrollReveal().reveal("#feature .f-logo", {
     duration: 2000,
     origin: "bottom",
-    distance: "1rem",
+    distance: "10px",
     delay: 1000,
 });
 ScrollReveal().reveal("#footer .hero-logo", {
     duration: 2000,
     origin: "bottom",
-    distance: "1rem",
+    distance: "10px",
     delay: 1500,
 });
