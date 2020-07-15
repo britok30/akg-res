13d79370d578f3c51830dabacd30ea9edcc09e02
Kelvin Brito <britok30@gmail.com>
2020-07-14 21:03:19 -0400

    updates

---
+++ js/techscroll.js
@@ -0,0 +1,62 @@
+ScrollReveal().reveal(".hero-text-box", {
+    duration: 2000,
+    origin: "top",
+    distance: "30rem",
+});
+ScrollReveal().reveal(".navbar", {
+    duration: 2000,
+    origin: "bottom",
+});
+ScrollReveal().reveal(".about-us", {
+    duration: 2000,
+    origin: "top",
+    distance: "30rem",
+});
+ScrollReveal().reveal(".location", {
+    duration: 2000,
+    origin: "top",
+    distance: "30rem",
+});
+ScrollReveal().reveal("video", {
+    duration: 2000,
+    origin: "bottom",
+    distance: "40rem",
+    viewFactor: 0.2,
+});
+ScrollReveal().reveal(".hg-list", {
+    duration: 2000,
+    origin: "top",
+    distance: "30rem",
+});
+ScrollReveal().reveal(".exclusive", {
+    duration: 2000,
+    origin: "bottom",
+    distance: "30rem",
+});
+ScrollReveal().reveal("#showcase h2, p", {
+    duration: 2000,
+    origin: "bottom",
+    delay: 1000,
+});
+ScrollReveal().reveal("#gallery", {
+    duration: 2000,
+    origin: "bottom",
+    distance: "50rem",
+    delay: 1500,
+});
+ScrollReveal().reveal("#showcase-2 h2, p", {
+    duration: 2000,
+    origin: "bottom",
+    delay: 1000,
+});
+ScrollReveal().reveal("#feature .f-logo", {
+    duration: 2000,
+    origin: "bottom",
+    distance: "30rem",
+    delay: 1000,
+});
+ScrollReveal().reveal("#footer .hero-logo", {
+    duration: 2000,
+    origin: "bottom",
+    delay: 1500,
+});
