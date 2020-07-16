01a3de1e7d6715dc8a902ee028cfc21afedd9551
Kelvin Brito <britok30@gmail.com>
2020-07-15 20:07:44 -0400

    remove scrollbar/new img

--- js/parallax.js
+++ js/parallax.js
@@ -1,5 +1,27 @@
-// let img = document.getElementsByClassName("losangeles");
+let img = document.getElementsByClassName("losangeles");
 
-// new simpleParallax(img, {
-//     scale: 1.5,
-// });
+new simpleParallax(img);
+
+let img2 = document.getElementsByClassName("luxury-1");
+
+new simpleParallax(img2, {
+    orientation: "right",
+});
+
+let img3 = document.getElementsByClassName("luxury-2");
+
+new simpleParallax(img3, {
+    orientation: "up",
+});
+
+let img4 = document.getElementsByClassName("luxury-3");
+
+new simpleParallax(img4, {
+    orientation: "down",
+});
+
+let img5 = document.getElementsByClassName("showcase");
+
+new simpleParallax(img5, {
+    orientation: "up",
+});
